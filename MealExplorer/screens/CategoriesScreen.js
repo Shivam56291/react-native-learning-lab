4f86643195c6c47efb4c14d2682ef229c73fa9c5
Shivam Kumar <roysinghaniashivam@gmail.com>
2026-01-01 14:30:57 +0530

    feat(MealExplorer): add category grid and screen navigation
    - Set up React Navigation and default screen
    - Displayed meal categories in a grid
    - Implemented navigation between category and meals screens
    - Added models, dummy data, and CategoryGridTile

---
+++ MealExplorer/screens/CategoriesScreen.js
@@ -0,0 +1,35 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+export default function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
+    }
+
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
